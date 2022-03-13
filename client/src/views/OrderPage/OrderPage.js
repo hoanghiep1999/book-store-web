09c1d8c6f7617cd231a0b0e6ddc2748874873e15
hiep1999hppro <hoanghiep1999hp@gmail.com>
2022-03-13 10:30:39 +0700

    update code

--- client/src/views/OrderPage/OrderPage.js
+++ client/src/views/OrderPage/OrderPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Loading from '../../components/Loading/Loading';
 
 import './OrderPage.css';
 
@@ -15,7 +16,7 @@ export default function OrderPage ({children}) {
 
   useEffect(() => {
     axios.get(`https://dhh-book-store-app.herokuapp.com/api/order/${id}`)
-      .then(res => setOrderData(res.data))
+      .then(res => setTimeout(() => setOrderData(res.data), 2500))
       .catch(err => {
         throw err;
       })
@@ -29,62 +30,65 @@ export default function OrderPage ({children}) {
           <div className="order-page-back">
             <Link to="/"><i className="fa-solid fa-left-long"></i>Back to homepage</Link>
           </div>
-          <h3>Order Received</h3>
-          <h4>Thank you. Your order has been received.</h4>
           {
-            orderData && <>
-            <div className="order-info">
-              <div className="order-info-group">
-                <span>Order ID</span>
-                <span>{orderData._id}</span>
+            orderData ? <>
+              <h3>Order Received</h3>
+              <h4>Thank you. Your order has been received.</h4>
+              <div className="order-info">
+                <div className="order-info-group">
+                  <span>Order ID</span>
+                  <span>{orderData._id}</span>
+                </div>
+                <div className="order-info-group">
+                  <span>Date</span>
+                  <span>{orderData.createAt.slice(0,10).split('-').reverse().join('/') + "  "} 
+                    {new Number(orderData.createAt.slice(11,19).split(':')[0]) + 7 + ":" + orderData.createAt.slice(11,19).split(':')[1] + ":" + orderData.createAt.slice(11,19).split(':')[2]}
+                  </span>
+                </div>
+                <div className="order-info-group">
+                  <span>Total</span>
+                  <span>{`$${orderData.total.$numberDecimal}`}</span>
+                </div>
+                <div className="order-info-group">
+                  <span>Payment method</span>
+                  <span>{orderData.paymentMethod}</span>
+                </div>
               </div>
-              <div className="order-info-group">
-                <span>Date</span>
-                <span>{orderData.createAt.slice(0,10).split('-').reverse().join('/')}</span>
+              <div className="order-details">
+                <span>Order details</span>
+                <ul>
+                  <li>
+                    <span>Total Item</span>
+                    <span>:</span>
+                    <span>{orderData.products.length} items</span>
+                  </li>
+                  <li>
+                    <span>Name</span>
+                    <span>:</span>
+                    <span>{orderData.contactInfo.name}</span>
+                  </li>
+                  <li>
+                    <span>Email</span>
+                    <span>:</span>
+                    <span>{user.userEmail}</span>
+                  </li>
+                  <li>
+                    <span>Address</span>
+                    <span>:</span>
+                    <span>{orderData.contactInfo.address}, {orderData.contactInfo.country}</span>
+                  </li>
+                  <li>
+                    <span>Phone</span>
+                    <span>:</span>
+                    <span>{orderData.contactInfo.phone}</span>
+                  </li>
+                </ul>
               </div>
-              <div className="order-info-group">
+              <div className="order-total">
                 <span>Total</span>
                 <span>{`$${orderData.total.$numberDecimal}`}</span>
-              </div>
-              <div className="order-info-group">
-                <span>Payment method</span>
-                <span>{orderData.paymentMethod}</span>
-              </div>
-            </div>
-            <div className="order-details">
-              <span>Order details</span>
-              <ul>
-                <li>
-                  <span>Total Item</span>
-                  <span>:</span>
-                  <span>{orderData.products.length} items</span>
-                </li>
-                <li>
-                  <span>Name</span>
-                  <span>:</span>
-                  <span>{orderData.contactInfo.name}</span>
-                </li>
-                <li>
-                  <span>Email</span>
-                  <span>:</span>
-                  <span>{user.userEmail}</span>
-                </li>
-                <li>
-                  <span>Address</span>
-                  <span>:</span>
-                  <span>{orderData.contactInfo.address}, {orderData.contactInfo.country}</span>
-                </li>
-                <li>
-                  <span>Phone</span>
-                  <span>:</span>
-                  <span>{orderData.contactInfo.phone}</span>
-                </li>
-              </ul>
-            </div>
-            <div className="order-total">
-              <span>Total</span>
-              <span>{`$${orderData.total.$numberDecimal}`}</span>
-            </div></>
+              </div></> :
+              <Loading />
           }
         </div>
       </div>
